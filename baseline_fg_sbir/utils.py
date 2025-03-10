993c73e7e56a84c8b62dd529fbcb5e17f298104a
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-10 10:30:55 +0700

    update main

--- baseline_fg_sbir/utils.py
+++ baseline_fg_sbir/utils.py
@@ -3,10 +3,10 @@ import torchvision.transforms as transforms
 def get_transform(type):
     if type == 'train':
         transform_list = [
-            transforms.RandomResizedCrop(299, scale=(0.85, 1.0)),
             transforms.RandomHorizontalFlip(0.5),
             transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.15),
             transforms.RandomRotation(15),
+            transforms.Resize(299),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]
